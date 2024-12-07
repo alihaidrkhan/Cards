c63aa77dc0947fad09103f611b7861b283095c60
alihaidrkhan <161750681+alihaidrkhan@users.noreply.github.com>
2024-12-07 11:50:06 -0800

    first commit

---
+++ src/app/Components/Cards.tsx
@@ -0,0 +1,21 @@
+export default function Cards (){
+    return(
+        <div className="grid grid-cols-3 place-items-center">
+            <div className="bg-black h-48 w-52 py-6 my-6">
+                <h1 className="text-white text-lg font-bold text-center py-4">Perfume</h1>
+                <h4 className="text-white text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta?</h4>
+            </div>
+
+        <div className="bg-orange-500 h-48 w-52 py-6 my-6">
+                <h1 className="text-white text-lg font-bold text-center py-4">Perfume</h1>
+                <h4 className="text-white text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta?</h4>
+            </div>
+
+            <div className="bg-gray-500 h-48 w-52 py-6 my-6">
+                <h1 className="text-white text-lg font-bold text-center py-4">Perfume</h1>
+                <h4 className="text-white text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta?</h4>
+            </div>                
+        
+        </div>
+    )
+}
